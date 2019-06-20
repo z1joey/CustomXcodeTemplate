9a2a5901084d9f929478ce0d40c73cf5894ff160
Joey Zhang <joey.zhang@tgitech.net>
2019-06-20 09:25:35 +0800

    feat: add a template file

---
+++ Template/___FILEBASENAME___.swift
@@ -0,0 +1,23 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+    
+    // MARK: - IBOutlet properties
+    
+    // MARK: - IBActions
+    
+}
+
+
+// MARK:- Override methods
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
